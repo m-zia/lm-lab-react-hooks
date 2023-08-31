b2597af84dd9e4091dd62891bacd307dda8f66b6
M Zia <murtaza_zia@hotmail.co.uk>
2023-08-31 21:10:39 +0100

    completed custom hook exercise

---
+++ src/components/hooks/use_x/use_todo.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { isError } from '../../../helpers/is_error';
+
+/** This is the response that TypiCode gives for the /todos/ endpoint */
+interface TodoResponse {
+	userId: number;
+	id: number;
+	title: string;
+	completed: boolean;
+}
+
+export const use_todo = (url :string) => {
+
+    const [data, setData] = useState<TodoResponse>();
+    const [isFetching, setIsFetching] = useState(true);
+
+
+    useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(url);
+				setIsFetching(false);
+				if (response.status === 200) {
+					const json = await response.json();
+					setData(json);
+				}
+			} catch (e: unknown) {
+				setIsFetching(false);
+
+				console.log(isError(e) ? e.message : 'Unknown error!');
+			}
+		};
+		fetchData();
+	}, [url]);
+
+    return {data, isFetching}
+}
+
+export default use_todo;
