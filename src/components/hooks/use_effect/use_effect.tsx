0e591e5bab2b902096f5362d1acd6fc691606397
M Zia <murtaza_zia@hotmail.co.uk>
2023-08-31 18:18:02 +0100

    completed use_effect exercise

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,40 @@
 // make an api call using useEffect
 
+import { useEffect, useState } from "react"
+import axios from 'axios';
+
+interface Todo {
+    "userId": number,
+    "id": number,
+    "title": string,
+    "completed": boolean
+}
+
+
 export const APICall = () => {
 
-    // Your code here!
+    const [json, setJson] = useState<Todo | null>(null);
+
+    useEffect(() => {
+        async function fetchTodo() {
+            try {
+                const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+                const todo = response.data;
+                setJson(todo);
+            } catch (error) {
+                console.error('Error fetching todo:', error);
+            }
+        }
+
+        fetchTodo()
+
+    }, []);
 
     return (
         <>
             <h2>useEffect</h2>
-            <p>Your todo here!</p>
+
+            <p>{json ? json.title : 'No todo to show' }</p>
         </>
     )
 }
