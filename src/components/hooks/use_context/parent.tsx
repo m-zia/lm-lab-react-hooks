bed3bc25ef08bb1dd4799e0e5dd2a4aaf96d5cb9
M Zia <murtaza_zia@hotmail.co.uk>
2023-08-31 17:51:42 +0100

    completed use_context exercise

--- src/components/hooks/use_context/parent.tsx
+++ src/components/hooks/use_context/parent.tsx
@@ -1,10 +1,27 @@
 import { useState } from 'react';
 import { Child1 } from './child_1';
 import { Child2 } from './child_2';
+import { createContext } from 'react';
+
+
+export const ThemeContext = createContext({
+    backgroundColor: "",
+    color: "",
+    padding: "",
+    margin: ""
+})
+
 
 export const Parent = () => {
 
     const [darkTheme, setDarkTheme] = useState(true)
+
+    const theme = {
+        backgroundColor: darkTheme ? '#333' : '#CCC',
+        color: darkTheme ? 'white' : '#333',
+        padding: '2rem',
+        margin: '2rem',
+    };
 
     const toggleTheme = () => {
         setDarkTheme(prevDarkTheme => !prevDarkTheme)
@@ -14,8 +31,10 @@ export const Parent = () => {
         <div className='section'>
             <h2>useContext</h2>
             <button onClick={toggleTheme}>Toggle Theme</button>
-            <Child1 dark={darkTheme} />
-            <Child2 dark={darkTheme} />
+            <ThemeContext.Provider value={theme}>
+                <Child1 />
+                <Child2 />
+            </ThemeContext.Provider>
         </div>
 
     )
